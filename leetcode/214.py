347b7f0f4b3762d207c39eebfb19c5c4a29a4bb4
kimtaesu <kimtaesoo188@gmail.com>
2019-07-17 19:11:46 +0900

    add

--- leetcode/214.py
+++ leetcode/214.py
@@ -1,16 +1,15 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-
-        result = []
-        front = 0
+        i = 0
         for i in range(len(s) - 1, 0, -1):
-            if s[front] == s[i]:
-                result.ad
-                continue
-            else:
-                s[front]
-                front += 1
-            front += 1
+            if s[i] != s[i]:
+                i += 1
+        if i == len(s):
+            return s
+
+        remain_rev = s[i:len(s)]
+        remain_rev + self.shortestPalindrome(s[0:i] + s[i])
+        print(remain_rev)
 
 
 Solution().shortestPalindrome("aacecaaa")
